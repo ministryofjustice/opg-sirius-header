c8ae2b319d87b65d25101ba4af2b0c4fc8223b09
Kate <kate.smith@digital.justice.gov.uk>
2023-06-13 14:14:23 +0100

    sw-5332 loads static file with html info when running main.go

--- main.go
+++ main.go
@@ -1,22 +1,73 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"os"
 )
 
-func main() {
+type Template interface {
+	ExecuteTemplate(io.Writer, string, interface{}) error
+}
+
+func renderTemplate(w http.ResponseWriter, name string, data interface{}) {
+	// This is inefficient - it reads the templates from the
+	// filesystem every time. This makes it much easier to
+	// develop though, so I can edit my templates and the
+	// changes will be reflected without having to restart
+	// the app.
+	t, err := template.ParseGlob("template/*.html")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error %s", err.Error()), 500)
+		return
+	}
+
+	err = t.ExecuteTemplate(w, name, data)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error %s", err.Error()), 500)
+		return
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index.html", struct {
+		Name string
+	}{
+		Name: "Sonic the Hedgehog",
+	})
+}
+
+func logreq(f func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("path: %s", r.URL.Path)
+
+		f(w, r)
+	})
+}
+
+type App struct {
+	Port string
+}
+
+func (a *App) Start() {
 	port := getEnv("PORT", "3456")
 	prefix := getEnv("PREFIX", "")
 	logger := log.New(os.Stdout, "opg-sirius-header ", log.LstdFlags)
-	http.HandleFunc(prefix + "/", HelloServer)
+
+	http.Handle(prefix+"/", logreq(index))
+
 	logger.Println("Sirius header running at port " + port)
-	logger.Fatal(http.ListenAndServe(":" + port, nil))
+	logger.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "web/template/index.html")
+func main() {
+	server := App{
+		Port: getEnv("PORT", "3456"),
+	}
+	server.Start()
 }
 
 func getEnv(key, def string) string {

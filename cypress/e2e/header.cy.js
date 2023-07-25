96bf93d044f474cf1353721c81e43cfc778d1bf0
samuel.joseph <denialanderror@googlemail.com>
2023-07-25 17:08:35 +0100

    sw-6708: Refactor and adds conditional finance tab

--- cypress/e2e/header.cy.js
+++ cypress/e2e/header.cy.js
@@ -1,46 +1,75 @@
-describe('header spec', () => {
-
-  beforeEach(() => {
-    cy.visit('../index.html')
-  })
-
-  it('has all of the expected information within the header', () => {
-      cy.get('.govuk-header__link--homepage').should('be.visible');
-      cy.get('.govuk-header__link--homepage').should('contain.text', 'OPG');
-      cy.get('.govuk-header__service-name').should('be.visible');
-      cy.get('.govuk-header__service-name').should('contain.text', 'Sirius');
+describe("header spec", () => {
+  it("has all of the expected information within the header", () => {
+    cy.visit("index.html");
+    cy.contains(".govuk-header__link--homepage", "OPG");
+    cy.contains(".govuk-header__service-name", "Sirius");
   });
 
-  const expectedTitle = ["Power of Attorney", "Supervision", "Admin", "Sign out"];
+  const expectedTitle = [
+    "Power of Attorney",
+    "Supervision",
+    "Admin",
+    "Sign out",
+  ];
   const expectedUrl = ["/lpa", "/Supervision", "/Admin", "/Logout"];
 
   it("has working nav links within header banner", () => {
-      cy.get('#header-navigation')
-        .children()
-        .each(($el, index) => {
-          cy.wrap($el).should("contain", expectedTitle[index]);
-          const $expectedLinkName = expectedUrl[index].toLowerCase();
-          cy.wrap($el)
-              .find("a")
-              .should("have.attr", "href")
-              .and("contain", `${$expectedLinkName}`);
-        });
+    cy.visit("index.html");
+    cy.get("#header-navigation")
+      .children()
+      .each(($el, index) => {
+        cy.wrap($el).should("contain", expectedTitle[index]);
+        const $expectedLinkName = expectedUrl[index].toLowerCase();
+        cy.wrap($el)
+          .find("a")
+          .should("have.attr", "href")
+          .and("contain", `${$expectedLinkName}`);
+      });
   });
 
-  it('has all the expected links within the secondary nav list', () => {
-
+  it("has all the expected links within the secondary nav list", () => {
+    cy.visit("index.html");
     const expectedTitle = ["Create client", "Workflow", "Finance"];
-    const expectedUrl = ["/supervision/#/clients/search-for-client", "/supervision/workflow", "/supervision/#/finance-hub/reporting"];
+    const expectedUrl = [
+      "/supervision/#/clients/search-for-client",
+      "/supervision/workflow",
+      "/supervision/#/finance-hub/reporting",
+    ];
+    cy.get("#header-navigation")
+      .get(".moj-primary-navigation__list")
+      .children()
+      .each(($el, index) => {
+        cy.wrap($el).should("contain", expectedTitle[index]);
+        const $expectedLinkName = expectedUrl[index].toLowerCase();
+        cy.wrap($el)
+          .find("a")
+          .should("have.attr", "href")
+          .and("contain", `${$expectedLinkName}`);
+      });
+  });
 
-    cy.get('.moj-primary-navigation__list')
-        .children()
-        .each(($el, index) => {
-          cy.wrap($el).should("contain", expectedTitle[index]);
-          const $expectedLinkName = expectedUrl[index].toLowerCase();
-          cy.wrap($el)
-              .find("a")
-              .should("have.attr", "href")
-              .and("contain", `${$expectedLinkName}`);
-        });
-    });
+  it("highlights the current page nav", () => {
+    cy.visit("/supervision/workflow");
+    cy.get("#header-navigation")
+      .contains("Supervision")
+      .should("not.be.visible");
+    cy.get(".moj-primary-navigation__list > .moj-primary-navigation__item")
+      .contains("Workflow")
+      .should("have.attr", "aria-current", "page");
+
+    cy.visit("/supervision");
+    cy.get("#header-navigation")
+      .contains("Supervision")
+      .should("not.be.visible");
+    cy.get(".moj-primary-navigation__list > .moj-primary-navigation__item")
+      .contains("Workflow")
+      .should("not.have.attr", "aria-current");
+  });
+
+  it("hides the finance tab when not set to show", () => {
+    cy.visit("/supervision/workflow");
+    cy.get(".moj-primary-navigation__list")
+      .contains("Finance")
+      .should("be.hidden");
+  });
 });
